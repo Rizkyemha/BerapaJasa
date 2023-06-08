4bf8439cf852a5ae1e0ef1be00909ddaa79030fb
wisnuarizky <wisnuarizky01@gmail.com>
2023-06-08 21:33:20 +0700

    membuat tampilan sign in dan sign up pada mobile

--- src/scripts/views/components/login/signin.js
+++ src/scripts/views/components/login/signin.js
@@ -5,18 +5,24 @@ class SigninCreator extends HTMLElement {
 
   render() {
     // eslint-disable-next-line no-return-assign
-    return this.innerHTML = `
-    <a href="#/dashboard">back-button</a>
+    return (this.innerHTML = `
+    <div class="back-button">
+      <button>
+        <a href="#/dashboard"><i class="fa-solid fa-arrow-left"></i></a>
+      </button>
+    </div>
       <div class="signin-section__container">
           <h3 class="signin-section__title">Sign In</h3>
           <div class="signin-section__input-container">
-              <input type="text" class="input__email">
-              <input type="password" class="input__password">
-              <a href="#/signup" class="button__signup">Sign up</a>
-              <button class="button__signin">Sign In</button>
+              <input type="text" class="input__email" placeholder="Username">
+              <input type="password" class="input__password" placeholder="Password">
+          </div>
+          <div class="button-container">
+            <button class="button__signup"><a href="#/signup"">Sign up</a></button>
+            <button class="button__signin">Login</button>
           </div>
       </div>
-      `;
+      `);
   }
 }
 
--- src/scripts/views/components/login/signup.js
+++ src/scripts/views/components/login/signup.js
@@ -5,32 +5,41 @@ class SignupCreator extends HTMLElement {
 
   render() {
     // eslint-disable-next-line no-return-assign
-    return this.innerHTML = `
-    <a href="#/dashboard">back-button</a>
+    return (this.innerHTML = `
+    <div class="back-button">
+      <button>
+        <a href="#/dashboard"><i class="fa-solid fa-arrow-left"></i></a>
+      </button>
+    </div>
     <div class="signup-section__container">
         <h3 class="signup-section__title">Sign Up</h3>
         <div class="signup-section__input-container">
-            <input type="text" class="input__name">
-            <input type="text" class="input__name">
-            <input type="text" class="input__email">
-            <input type="password" class="input__password">
+          <div class="area-name">        
+            <input type="text" class="input__name" placeholder="First name">
+            <input type="text" class="input__name" placeholder="Last name">
+          </div>
+            <input type="text" class="input__email" placeholder="Email">
+            <input type="password" class="input__password" placeholder="Password">
+            <input type="password" class="input__password" placeholder="Validasi Password">
             <div class="container-gender">
               <label class="item-gender">
-                <input type="checkbox" value="Laki-laki">
+                <input type="radio" value="Laki-laki">
                 <span>Laki-laki</span>
               </label>
               <label>
-                <input type="checkbox" value="Perempuan">
+                <input type="radio" value="Perempuan">
                 <span>Perempuan</span>
               </label>
             </div>
-            <input type="date">
-            <input type="text">
-            <a href="#/signin">Sign in</a>
-            <button>Register</button>
+            <input type="date" class="tanggal">
+            <textarea name="" class="alamat" id="" cols="30" rows="10"></textarea>
+            <div class="button-container-regis">
+              <button class="signin"><a href="#/signin">Sign in</a></button>
+              <button class="regis">Register</button>
+            </div>
         </div>
     </div>
-    `;
+    `);
   }
 }
 
