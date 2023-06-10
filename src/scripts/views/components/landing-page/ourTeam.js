5761fae23522bef27a584484ee870dabfdcbe4a8
wisnuarizky <wisnuarizky01@gmail.com>
2023-06-10 12:02:28 +0700

    membuat responsif pada bagian our team

--- src/scripts/views/components/landing-page/ourTeam.js
+++ src/scripts/views/components/landing-page/ourTeam.js
@@ -11,28 +11,28 @@ class ProductAboutUs extends HTMLElement {
           <p class="team__slogan">Alone you are great, but together you’ll be stronger”</p>
           <team-items class="team__items">
                 <article class="team__item">
-                    <div class="team__item__img-rizky"></div>
+                    <img class="team__item-image" src="./image/rizky.jpg" alt="" />
                     <div class="team__item__info">
                         <h4 class="team__item__info__name">M. Rizky Mahendra</h4>
                         <p  class="team__item__info__role">Back-End || PM</p>
                     </div>
                 </article>
                 <article class="team__item">
-                    <div class="team__item__img-fadiya"></div>
+                    <img class="team__item-image" src="./image/fadiya.jpg" alt="" />
                     <div class="team__item__info">
                         <h4 class="team__item__info__name">Fadiya Qurotun N</h4>
                         <p  class="team__item__info__role">Front-End</p>
                     </div>
                 </article>
                 <article class="team__item">
-                    <div class="team__item__img-wisnu"></div>
+                    <img class="team__item-image" src="./image/wisnu.jpg" alt="" />
                     <div class="team__item__info">
                         <h4 class="team__item__info__name">Wisnu Arizky k</h4>
                         <p  class="team__item__info__role">Front-End</p>
                     </div>
                 </article>
                 <article class="team__item">
-                    <div class="team__item__img-ivana"></div>
+                <img class="team__item-image" src="./image/ivana.jpg" alt="" />
                     <div class="team__item__info">
                         <h4 class="team__item__info__name">Ivana Hayyu</h4>
                         <p  class="team__item__info__role">UI / UX Desainer</p>
