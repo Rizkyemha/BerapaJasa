f4ad63bc142beb8d51ea3ce69a2db2b1cd856dd3
Fadiyaqp <fadiyaqp@gmail.com>
2023-06-15 20:15:15 +0700

    menambahkan dan mengubah landing page footer dan product serta style pada masing masing ukuran

--- src/scripts/views/components/landing-page/footer.js
+++ src/scripts/views/components/landing-page/footer.js
@@ -2,18 +2,23 @@ const Footer = {
   render() {
     return `
     <footer>
-    <section class="ft-sosmed">
-      <ul class="ft-sosmed-list">
-        <li><a href=""><i class="fa-regular fa-envelope"></i></a></li>
-        <li><a href=""><i class="fa-brands fa-github"></i></a></li>
-        <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
-      </ul>
-      <p>BerapaJasa</p>
-      <ul>
-        <li class="watermark">Copyright © Kelompok 6, 2023. Dicoding</li>
-      </ul>
-    </section>
-  </footer>
+    <div class="footer-isi">
+      <div class="footer-image"></div>
+        <div class="footer-tulisan">
+        <ul>
+          <li><a href="https://github.com/Rizkyemha/BerapaJasa" target="_blank" rel="noreferrer">BerapaJasa</a></li>
+          <li><a href="https://github.com/Rizkyemha" target="_blank" rel="noreferrer">M Rizky Mahendra</a></li>
+          <li><a href="https://github.com/Fadiyaqp" target="_blank" rel="noreferrer">Fadiya Qistina Putri</a></li>
+          <li><a href="https://github.com/wisnuarizky" target="_blank" rel="noreferrer">Wisnu Arizky</a></li>
+          <li><a href="https://github.com/ivanaHayyu" target="_blank" rel="noreferrer">Ivana Hayyu</a></li>
+        </ul>
+        </div>
+      </div>
+      <div class="watermark">
+      <p>Copyright © Kelompok 6 Berapa Jasa, 2023. Dicoding</p>
+    </div>
+      
+    </footer>
     `;
   },
 };
--- src/scripts/views/components/landing-page/product.js
+++ src/scripts/views/components/landing-page/product.js
@@ -9,7 +9,7 @@ class ProductCreator extends HTMLElement {
         <h2>What Can We Do ?</h2>
         <div class="service-items">
             <div class="service-item">
-                <div class="image-pinjaman"></div>
+                <div class="image-pinjaman-1"></div>
                 <h3>Pinjaman Flat</h3>
                 <p>Jenis pinjaman di mana jumlah cicilan 
                 yang harus dibayarkan setiap bulannya tetap.</p>
@@ -18,7 +18,7 @@ class ProductCreator extends HTMLElement {
                 </div>
             </div>
             <div class="service-item">
-                <div class="image-pinjaman"></div>
+                <div class="image-pinjaman-2"></div>
                 <h3>Pinjaman Efektif</h3>
                 <p>jenis pinjaman di mana bunga yang harus dibayar oleh peminjam 
                 dihitung berdasarkan saldo pinjaman yang masih beredar setiap bulannya.</p>
@@ -27,7 +27,7 @@ class ProductCreator extends HTMLElement {
                 </div>
             </div>
             <div class="service-item">
-                <div class="image-pinjaman"></div>
+                <div class="image-pinjaman-3"></div>
                 <h3>Pinjaman Anuitas</h3>
                 <p>Jenis Pinjaman dimana pembayaran cicilan setiap bulannya tetap, namun 
                 proporsi antara bagian bunga dan pokok dalam cicilan berubah seiring waktu.</p>
